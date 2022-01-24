95bc16eda8feedc6768093cd8525924a4cbb6845
Albert Domínguez <albertdc1984@gmail.com>
2022-01-24 21:48:38 +0100

    Separated js files for each class and linked with Personaje

---
+++ js/rey.js
@@ -0,0 +1,14 @@
+import Personaje from "./personaje";
+
+class Rey extends Personaje {
+  anyosReinado;
+
+  constructor(nombreRey, familiaRey, edadRey, anyosReinadoRey) {
+    super(nombreRey, familiaRey, edadRey);
+    this.anyosReinado = anyosReinadoRey;
+  }
+
+  comunicar() {
+    return `${super.comunicar()}Vais a morir todos`;
+  }
+}
