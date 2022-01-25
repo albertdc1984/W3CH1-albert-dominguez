a928d1a4143621d7eecc2fa89301f00b3d8ab88f
Albert Domínguez <albertdc1984@gmail.com>
2022-01-25 22:52:41 +0100

    Merged branch but forgot to commit!

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import Card from "./components/card.js";
+import ImugClass from "./components/image.js";
 
 const bronnImg = "/img/bronn.jpg";
 const daenerysImg = "/img/daenerys.jpg";
@@ -8,6 +9,7 @@ const tyrionImg = "/img/tyrion.jpg";
 const cardContainer = document.querySelector("ul");
 
 const bronnCard = new Card(cardContainer, "character col", bronnImg);
+const daenerysPicture = new ImugClass(daenerysImg);
 const jaimeCard = new Card(cardContainer, "character col", daenerysImg);
 const daenerysCard = new Card(cardContainer, "character col", tyrionImg);
 const tyrionCard = new Card(cardContainer, "character col", joffreyImg);
